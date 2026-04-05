4127da23b5636eff2393bd18753892f42c90f712
Blanca Beuste Chavarri <blanca.beuste.dp@gmail.com>
2026-04-05 15:52:53 +0200

    Incluyo paleta de colores y tipografías

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,9 @@
     <title><?= $title ?? "ATM Sin Tension" ?></title>
     <!-- links de css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="./public/css/estilos.css">
+    <link rel="stylesheet" href="./public/css/token.css">
+    <link rel="stylesheet" href="./public/css/utilities.css">
+    <link rel="stylesheet" href="./public/css/main.css">
     <!-- CSS extra por página, veremos si lo usamos -->
     <?php if (!empty($links_css)):
         foreach ($links_css as $css): ?>
