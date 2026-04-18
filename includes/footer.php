0cf6ad0525018b865270fda5bbe557cf82be8e56
Blanca Beuste Chavarri <blanca.beuste.dp@gmail.com>
2026-04-19 00:17:07 +0200

    Añado el footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,16 @@
+<div class="container-fluid bg-primario p-0"> 
+    <footer class="py-3 mt-4"> 
+        <ul class="nav justify-content-center pb-3 mb-3"> 
+            <li class="nav-item"><a href="/blancaTFG/index.php" class="text-secundario nav-link px-2 text-body-secondary">Inicio</a></li> 
+            <li class="nav-item"><a href="/blancaTFG/servicios.php" class="text-secundario nav-link px-2 text-body-secondary">Servicios</a></li>
+            <li class="nav-item"><a href="/blancaTFG/sobreMi.php" class="text-secundario nav-link px-2 text-body-secondary">Sobre mí</a></li>
+            <li class="nav-item"><a href="/blancaTFG/contacto.php" class="text-secundario nav-link px-2 text-body-secondary">Contacto</a></li>
+            <li class="nav-item"><a href="#" class="text-secundario nav-link px-2 text-body-secondary">Iniciar sesión</a></li>
+            <li class="nav-item"><a href="#" class="text-secundario nav-link px-2 text-body-secondary">Registrarse</a></li>
+        </ul>
+            <p class="text-secundario text-center text-body-secondary">© 2026 ATM Sin Presion</p>
+            <p class="text-secundario text-center text-body-secondary">Diseño de Blanca Beuste</p>
+     </footer>
+ </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</html>
